326b22a336126511dc22c5acc67c226bf85de3f5
Mike Seeley <michael.j.seeley@gmail.com>
2015-01-22 12:10:39 -0600

    Update of string formatting.
    Update of string .format() for IP address Var naming.

--- arin_search.py
+++ arin_search.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 ip = raw_input('Enter an IP: ')
 
-arin_html = requests.get('http://whois.arin.net/rest/ip/{formatted_ip_addr}'.format(formatted_ip_addr=ip))
+arin_html = requests.get('http://whois.arin.net/rest/ip/{ip_addr}'.format(ip_addr=ip))
 
 arin_soup = BeautifulSoup(arin_html.content)
 if arin_html.status_code == 200:
@@ -15,6 +15,7 @@ if arin_html.status_code == 200:
     print ('\n' + str(c_name[0][1]))
     print arin_soup.startaddress.get_text() + '/' + arin_soup.cidrlength.get_text() + \
         ' - ' + arin_soup.endaddress.get_text()
-    print '\nResults from: http://whois.arin.net/rest/ip/{formatted_ip_addr}'.format(formatted_ip_addr=ip)
+    print '\nResults from: http://whois.arin.net/rest/ip/{ip_addr}'.format(ip_addr=ip)
 else:
     print ('Error connecting to whois.arin.net')
+    
